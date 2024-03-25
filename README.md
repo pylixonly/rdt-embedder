386279b1fd80867646063a45a1b7b158e034fdc7
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2024-03-25 11:43:32 +0800

    Moar stuff

---
+++ README.md
@@ -0,0 +1,7 @@
+# rdt-embedder
+
+## Usage
+Load `dist.js` before React. Access exports through `window.__reactDevToolsCore.exports`.
+
+## Build
+`pnpm bundle` to build `dist.js`.
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-export * from "react-devtools-core";
+import * as reactDevToolsCore from "react-devtools-core";
+
+globalThis.__reactDevTools = {
+    version: "5.0.2", // Sync with package.json
+    exports: reactDevToolsCore
+};
