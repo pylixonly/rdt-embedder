641e24671ba816efa4299ebee1f98aa2f4468cff
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2024-08-29 16:07:31 +0800

    shim window.console

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,15 +2,17 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 
+const consoleShimScript = "var console = new Proxy({}, { get: () => () => void 0 });"
+
 export default {
     input: 'index.js',
     output: {
         file: 'dist.js',
         format: 'iife',
         compact: true,
-        intro: "var window = globalThis, self = globalThis;",
+        intro: "var window = globalThis, self = globalThis, console = new Proxy({}, { get: () => () => void 0 });"
     },
-    plugins: [
+  plugins: [
         nodeResolve(),
         commonjs(),
         esbuild({ minify: true })
